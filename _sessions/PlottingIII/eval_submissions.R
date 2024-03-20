31d8d9385d5a872a023be7152a9f3102143d156e
Michael Schulte <michael.schulte@unibe.ch>
2024-03-20 10:17:47 +0100

    secondtolast

--- _sessions/PlottingIII/eval_submissions.R
+++ _sessions/PlottingIII/eval_submissions.R
@@ -18,9 +18,9 @@ formr_connect(email = 'michael.schulte@unibe.ch',
               password = 'pitzeb-0dybqo-gapkId',
               host = 'https://formr.imucb.ch')
 
-raw <- formr_raw_results('RDS', host = "https://formr.imucb.ch")%>%
+raw <- formr_raw_results('RDS', host = "https://formr.imucb.ch") %>%
   filter(!ended == 'NA',
-         ended > ymd('2023-02-23')) %>%
+         ended > ymd_hms('2024-03-12-22-40-00')) %>%
   filter(!is.na(session))
 
 df_raw <- 
@@ -41,14 +41,19 @@ raw %>%
 na.omit() %>%
   mutate(values = as.numeric(values))
 
+#joel.allgaier@students.unibe.ch
+#maria.fuchs@students.unibe.ch
+#olivia.demeny@students.unibe.ch
+#rafaela.himmelberger@students.unibe.ch
+#jan.zimmermann@students.unibe.ch
+
 # plot single result ----
 single <- ggplot(df_raw, aes(names, values)) +
- geom_point(stat = 'identity') + 
+ stat_summary(stat = mean, geom = 'point') + 
  facet_wrap(~student, nrow = 1) +
 
   labs(y = 'Bewertung',
        x = 'Bereich',
-       caption = caption,
        title = 'Ergebnisse Plotting Competition') +
   theme_bw()
 
@@ -58,6 +63,12 @@ overall_result <-
     group_by(names) %>%
     summarise(overall = mean(values),
               ci = sd(values))
+
+df_raw %>%
+  group_by(student) %>%
+    summarise(overall_mean = mean(values)) %>%
+  arrange(desc(overall_mean))
+
   
 overall_graph <- ggplot(overall_result, aes(reorder(names, -overall), overall)) +
   geom_point() +
